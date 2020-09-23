61253c87ba2983836ee800a7334ac7e7c3103deb
alijb <alijb@post.bgu.ac.il>
2020-09-23 18:34:44 +0300

    SignedUP user is in mongoDB

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,8 +2,22 @@ const User = require("../models/user");
 const shortId = require("shortid");
 
 exports.signup = (req, res) => {
+  User.findOne({ email: req.body.email }).exec((err, user) => {
+    if (user) {
+      return res.status(400).json({ error: "Email is taken" });
+    }
+  });
+
   const { name, email, password } = req.body;
-  res.json({
-    user: { name, email, password },
+  let username = shortId.generate();
+  let profile = `${process.env.CLIENT_URL}/profile/${username}`;
+  let newUSer = new User({ name, email, password, profile, username });
+  newUSer.save((err, success) => {
+    if (err) {
+      return res.status(400).json({ error: err });
+    }
+    // res.json({ user: success });
+    // });
+    res.json({ message: "Signup success! You Sign in" });
   });
 };
--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-const mongoose = require("mogoose");
+const mongoose = require("mongoose");
 const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema(
